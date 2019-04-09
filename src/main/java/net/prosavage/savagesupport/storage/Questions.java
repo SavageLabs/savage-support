78baea986b030b16f2f1a30d0f857d1c9b4a638c
ProSavage <naman0311@gmail.com>
2019-04-09 14:17:55 -0500

    Added permissions answer.

--- src/main/java/net/prosavage/savagesupport/storage/Questions.java
+++ src/main/java/net/prosavage/savagesupport/storage/Questions.java
@@ -31,5 +31,11 @@ public class Questions {
                         new MessageEmbed.Field("Step 3", "Use the command **/f claim *<radius>* safezone** to claim.\n*ex.   **/f claim 1 safezone***", false),
                         new MessageEmbed.Field("Extra Info", "You can use the command from **Step 3** to claim for any faction as an administrator.\n**/f claim <radius> <faction name>**\n*ex.   **/f claim 1 savagesupport***\n", false)
                 }, "", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Antu_flag-green.svg/512px-Antu_flag-green.svg.png"));
+        questionAnswerMap.put("permissions", new TaskListEmbed("Doing Permissions", new MessageEmbed.Field[]
+                {
+                        new MessageEmbed.Field("Step 1", "Make sure you are OP, or have the '*' permission if your permissions manager requires it.", false),
+                        new MessageEmbed.Field("Step 2", "[Click me to view permission nodes.](https://github.com/ProSavage/SavageFactions/wiki/SavageFactions-Permissions-List)", false),
+                        new MessageEmbed.Field("Tip", "You can use the permission kits to get most of the permissions we suggest players should have.", false)
+                }, "", "https://www.talkpoint.com/wp-content/uploads/ane-permissions-icon.png"));
     }
 }
