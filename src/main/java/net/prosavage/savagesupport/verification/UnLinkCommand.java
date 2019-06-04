943a9e45330dc1f52b296c92f9d6761e1fd60331
ProSavage <naman0311@gmail.com>
2019-06-04 17:31:26 -0500

    push unlink

--- src/main/java/net/prosavage/savagesupport/verification/UnLinkCommand.java
+++ src/main/java/net/prosavage/savagesupport/verification/UnLinkCommand.java
@@ -1,10 +1,7 @@
 package net.prosavage.savagesupport.verification;
 
 import net.dv8tion.jda.core.MessageBuilder;
-import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,10 +33,31 @@ public class UnLinkCommand {
         }
 
         Member member = members.get(0);
+        if (!member.getRoles().contains(getRole(guild, "verified"))) {
+            channel.sendMessage(new MessageBuilder("Member is not verified.").build()).queue();
+            return;
 
-        if (member.getRoles().contains(guild.getRolesByName("savagebuckets", true).get(0)) ||)
+        }
 
 
+        guild.getController().removeSingleRoleFromMember(member, guild.getRolesByName("verified", true).get(0)).queue();
+        channel.sendMessage(new MessageBuilder("Removed verified role! " + member.getEffectiveName()).build()).queue();
+
+
+        if (member.getRoles().contains(guild.getRolesByName("savagebuckets", true).get(0))
+                || member.getRoles().contains(guild.getRolesByName("savageftop", true).get(0))) {
+            guild.getController().removeRolesFromMember(member, guild.getRolesByName("savageftop", true).get(0)).queue();
+            guild.getController().removeRolesFromMember(member, guild.getRolesByName("savagebuckets", true).get(0)).queue();
+            channel.sendMessage(new MessageBuilder("Removed all premium plugin roles from " + member.getEffectiveName()).build()).queue();
+        }
+
+
+
+
+    }
+
+    private static Role getRole(Guild guild, String name) {
+        return guild.getRolesByName(name, true).get(0);
     }
 
 
